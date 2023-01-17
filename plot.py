0128ed9de3525c29ed5a320651a0bd2387d04daa
linlinlin97 <75768141+linlinlin97@users.noreply.github.com>
2023-01-16 20:55:40 -0500

    Add files via upload

--- plot.py
+++ plot.py
@@ -21,13 +21,13 @@ def summary(out_df, N_range, T_range, absolute = True, true = None):
     result['DME_MSE'] = np.log(result['DME_error']**2)
     NT_pairs = len(result.groupby(['N','T']).size())
     rep = int(len(result)/(5*NT_pairs))
-    result["estimand"] = (["MR"]*rep+["Direct"]*rep+["WIS1"]*rep+["WIS2"]*rep+["Baseline"]*rep)*NT_pairs #[""]none*rep
+    result["estimand"] = (["MR"]*rep+["Direct"]*rep+["IS1"]*rep+["IS2"]*rep+["Baseline"]*rep)*NT_pairs #[""]none*rep
     
     if absolute:
         for N in N_range:
             for T in T_range:
                 for error in [3,4,5,6]:
-                    for estimand in ['Baseline', 'Direct', 'MR', 'WIS1', 'WIS2']:
+                    for estimand in ['Baseline', 'Direct', 'MR', 'IS1', 'IS2']:
                         idx = result[(result['N'] == N) & (result['T'] == T) & (result['estimand']== estimand)].index
                         result.iloc[idx,error] = np.log(abs(result.iloc[idx,error].mean()))
     return result
@@ -37,59 +37,71 @@ def plot(result, x='NT'):
     fig, ((ax1, ax2, ax3, ax4) ,(ax5, ax6, ax7, ax8)) = plt.subplots(nrows=2, ncols=4, sharex=True, sharey=False, figsize = (10,5))
     
     COLORS = sns.color_palette("colorblind")
-    palette = {'MR' : COLORS[0],'Direct' : COLORS[1], 'Baseline': COLORS[2], 'WIS1': COLORS[3], 'WIS2': COLORS[4]}
+    palette = {'MR' : COLORS[0],'Direct' : COLORS[1], 'Baseline': COLORS[2], 'IS1': COLORS[3], 'IS2': COLORS[4]}
 
     ax1 = sns.lineplot(data=result, x=x, y="IDE_error", hue="estimand",
                        style="estimand", ci = 95, err_style="bars", ax = ax1,
                        n_boot = 20, palette = palette,linewidth = 2.0,
                        markers = True)
-    ax1.set_title('IDE')
-    ax1.axes.set_ylabel("logbias")
+    ax1.set_title('IDE', fontsize = 18)
+    ax1.tick_params(axis='y', labelsize=11)
+    ax1.axes.set_ylabel("logbias", fontsize = 18)
     
     ax2 = sns.lineplot(data=result, x=x, y="IME_error", hue="estimand",
                        style="estimand", ci = 95, err_style="bars", ax = ax2,
                        n_boot = 20, palette = palette,linewidth = 2.0,
                        markers = True)
-    ax2.set_title('IME')
+    ax2.set_title('IME', fontsize = 18)
+    ax2.tick_params(axis='y', labelsize=11)
     ax2.set(ylabel=None)
     
     ax3 = sns.lineplot(data=result, x=x, y="DDE_error", hue="estimand",
                        style="estimand", ci = 95, err_style="bars", ax = ax3,
                        n_boot = 20, palette = palette,linewidth = 2.0,
                        markers = True)
-    ax3.set_title('DDE')
+    ax3.set_title('DDE', fontsize = 18)
+    ax3.tick_params(axis='y', labelsize=11)
     ax3.set(ylabel=None)
     
     ax4 = sns.lineplot(data=result, x=x, y="DME_error", hue="estimand",
                        style="estimand", ci = 95, err_style="bars", ax = ax4,
                        n_boot = 20, palette = palette,linewidth = 2.0,
                        markers = True)
-    ax4.set_title('DME')
+    ax4.set_title('DME', fontsize = 18)
+    ax4.tick_params(axis='y', labelsize=11)
     ax4.set(ylabel=None)
 
     ax5 = sns.lineplot(data=result, x=x, y="IDE_MSE", hue="estimand",
                        style="estimand", ci = 95, err_style="bars",
                        ax = ax5, n_boot = 20, palette = palette,
                        linewidth = 2.0,markers = True)
-    ax5.axes.set_ylabel("logMSE")
+    ax5.axes.set_ylabel("logMSE", fontsize = 18)
+    ax5.tick_params(axis='x', labelsize=11)
+    ax5.axes.set_xlabel(x,fontsize = 12)
     
     ax6= sns.lineplot(data=result, x=x, y="IME_MSE", hue="estimand",
                        style="estimand", ci = 95, err_style="bars",
                        ax = ax6, n_boot = 20, palette = palette,
                        linewidth = 2.0,markers = True)
     ax6.set(ylabel=None)
+    ax6.tick_params(axis='x', labelsize=11)
+    ax6.axes.set_xlabel(x,fontsize = 12)
     
     ax7= sns.lineplot(data=result, x=x, y="DDE_MSE", hue="estimand",
                        style="estimand", ci = 95, err_style="bars",
                        ax = ax7, n_boot = 20, palette = palette,
                        linewidth = 2.0,markers = True)
     ax7.set(ylabel=None)
+    ax7.tick_params(axis='x', labelsize=11)
+    ax7.axes.set_xlabel(x,fontsize = 12)
     
     ax8= sns.lineplot(data=result, x=x, y="DME_MSE", hue="estimand",
                        style="estimand", ci = 95, err_style="bars",
                        ax = ax8, n_boot = 20, palette = palette,
                        linewidth = 2.0,markers = True)
     ax8.set(ylabel=None)
+    ax8.tick_params(axis='x', labelsize=11)
+    ax8.axes.set_xlabel(x,fontsize = 12)
     
     handles, labels = ax1.get_legend_handles_labels()
     lgd = fig.legend(handles, labels, loc='lower center', ncol = len(labels)
